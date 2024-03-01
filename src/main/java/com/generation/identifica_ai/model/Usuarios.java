b3493b07281c976ac6b2936ca48ff37bb3672565
Matheus Libanio <matheus.alexandre.libanio@gmail.com>
2024-03-01 20:39:36 -0300

    Revert "outra alteração de usuarios"
    This reverts commit 88ebe56c073dde88d095c9f9f142f9741ca8bc69.

--- src/main/java/com/generation/identifica_ai/model/Usuarios.java
+++ src/main/java/com/generation/identifica_ai/model/Usuarios.java
@@ -20,7 +20,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_usuarios")
-public class Usuarios {
+public class Usuario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
