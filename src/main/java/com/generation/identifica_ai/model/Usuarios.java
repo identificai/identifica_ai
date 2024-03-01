4786568b378fe66191a63a3edfa9f29522a42864
Matheus Libanio <matheus.alexandre.libanio@gmail.com>
2024-03-01 20:42:15 -0300

    revert

--- src/main/java/com/generation/identifica_ai/model/Usuarios.java
+++ src/main/java/com/generation/identifica_ai/model/Usuarios.java
@@ -20,7 +20,7 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_usuarios")
-public class Usuario {
+public class Usuarios {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
